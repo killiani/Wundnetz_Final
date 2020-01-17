0cecb7253af09b961e8fdb03f4488f59fdf1071d
Ben <59772495+killiani@users.noreply.github.com>
2020-01-17 09:16:43 +0100

    First Push

---
+++ templates/menu.php
@@ -0,0 +1,18 @@
+
+
+<div class="menu-close">
+	<a data-mobile-menu="close" href="#">×</a></div>
+<div class="nav_punkte">
+	<ul>
+		<li class="nav_punkte_einzeln"><h2>WUNDNETZ MITTELTHÜRINGEN E.V.</h2></li>
+		<hr><br><br>
+		<li class="nav_punkte_einzeln"><a  href="#article-6">STARTSEITE</a> </li>
+		<li class="nav_punkte_einzeln"><a  href="#article-6">MITGLIEDSCHAFT</a> </li>
+		<li class="nav_punkte_einzeln"><a  href="#article-6">INFORMATIONEN</a> </li>
+		<li class="nav_punkte_einzeln"><a  href="#article-6">FORUM</a> </li>
+		<li class="nav_punkte_einzeln"><a  href="#article-6">DOWNLOADS</a></li>
+		<li class="nav_punkte_einzeln"><a  href="#article-5">VORSTAND</a></li>
+		<li class="nav_punkte_einzeln"><a  href="#article-3">SATZUNG</a></li>
+	</ul>
+</div>
+
